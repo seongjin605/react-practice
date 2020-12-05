aac3d36b858cab1af496a3f591d6ec1416e6ce3f
preciouStar <seongjin605@users.noreply.github.com>
2020-12-05 19:07:34 +0900

    Test useEffect

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,11 +1,26 @@
 import React, {useState, useEffect} from 'react';
 
+/*
 function Test() {
   const [count, setCount] = useState(0);
   useEffect(() => {
     document.title = `업데이트 횟수: ${count}`;
   });
 
+  return <button onClick={() => setCount(count + 1)}>increase</button>;
+}
+*/
+
+function Test() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    function onClick() {
+      setCount(count + 1);
+      console.log('count:', count);
+    }
+    window.addEventListener("click", onClick);
+    return () => window.removeEventListener("click", onClick);
+  }, [count]);
   return <button onClick={() => setCount(count + 1)}>increase</button>;
 }
 
