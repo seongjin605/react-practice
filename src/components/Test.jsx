223edc44e02f7e5c92359da4d8bdabd009cccb2e
preciouStar <seongjin605@users.noreply.github.com>
2020-12-06 18:48:36 +0900

    Remove comment

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,16 +1,5 @@
 import React, {useState, useEffect} from 'react';
 
-/*
-function Test() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    document.title = `업데이트 횟수: ${count}`;
-  });
-
-  return <button onClick={() => setCount(count + 1)}>increase</button>;
-}
-*/
-
 function Test() {
   const [count, setCount] = useState(0);
   useEffect(() => {
