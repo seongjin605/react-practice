b9c4186851fa701e3d94fa4842d30b57fe4b1336
preciouStar <seongjin605@users.noreply.github.com>
2020-12-07 17:57:22 +0900

    Migration to class component

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,16 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Test() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    function onClick() {
-      setCount(count + 1);
-      console.log('count:', count);
-    }
-    window.addEventListener("click", onClick);
-    return () => window.removeEventListener("click", onClick);
-  }, [count]);
-  return <button onClick={() => setCount(count + 1)}>increase</button>;
+// function Test() {
+//   const [count, setCount] = useState(0);
+//   useEffect(() => {
+//     function onClick() {
+//       setCount(count + 1);
+//       console.log('count:', count);
+//     }
+//     window.addEventListener("click", onClick);
+//     return () => window.removeEventListener("click", onClick);
+//   }, [count]);
+//   return <button onClick={() => setCount(count + 1)}>increase</button>;
+// }
+
+class Test extends React.Component {
+  state = {
+    count1: 0,
+    count2: 0
+  };
+  onClick = () => {
+    const { count1 } = this.state;
+    this.setState({ count1: count1 + 1 });
+    console.log(count1);
+  };
+  render() {
+    window.addEventListener('click', this.onClick);
+    return <button onClick={() => this.onClick}>increase</button>;
+  }
 }
-
 export default Test;
