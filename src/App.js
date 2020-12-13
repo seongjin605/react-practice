85f200d71a1f8775a04140cc52127bd9932e57f3
preciouStar <seongjin605@users.noreply.github.com>
2020-12-14 00:50:28 +0900

    Remove comment

--- src/App.js
+++ src/App.js
@@ -24,8 +24,6 @@ function App() {
             </li>
           </ul>
         </nav>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/about">
             <About />
