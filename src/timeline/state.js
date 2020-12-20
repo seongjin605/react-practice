6057b32851932375c870d3ccdd79007f477e82fc
preciouStar <seongjin605@users.noreply.github.com>
2020-12-20 21:02:35 +0900

    Adding timeline  state

---
+++ src/timeline/state.js
@@ -0,0 +1,6 @@
+import createReducer from '../common/createReducer';
+
+const ADD = 'timeline/ADD';
+const REMOVE = 'timeline/REMOVE';
+const EDIT = 'timeline/EDIT';
+const INCREASE_NEXT_PAGE = 'timeline/INCREASE_NEXT_PAGE';
