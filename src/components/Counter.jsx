4ea5dfb3c840cfa2fc86e13ab08fec7fc65a6150
preciouStar <qkrtjdwls605@naver.com>
2021-03-07 21:36:15 +0900

    Test useEffect

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
   useEffect(() => {
     document.title = `업데이트 횟수: ${count}`;
-  });
-  return <button onClick={() => setCount(count + 1)}>increase</button>;
+    console.log('useEffect!!!');
+  },[count]);
+  return <>
+  <button onClick={() => setCount(count + 1)}>Plus count</button>
+  <button onClick={() => setCount2(count2 + 1)}>Plus count2</button>
+  </>
+  ;
 }
 
 export default Counter;
