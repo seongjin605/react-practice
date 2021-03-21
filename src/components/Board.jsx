3170506462cce12b43fba3cd71a91e3d79d09f56
preciouStar <qkrtjdwls605@naver.com>
2021-03-22 00:12:38 +0900

    Add pagination

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Posts from './board/Post';
+import Pagination from './board/Pagination';
 
 function Board() {
   const [posts, setPosts] = useState([]);
@@ -13,10 +14,12 @@ function Board() {
     setLoading(true);
     async function getPosts() {
       try {
-        const { data = [] } = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        setPosts(data);
-
-        return data;
+        const { data: posts = [] } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        // if (posts.length > 0) {
+        //   setPosts(posts.slice(0, 15));
+        // }
+        setPosts(posts);
+        return [posts];
       } catch (error) {
         console.error('getPosts error:', error);
       }
@@ -24,12 +27,20 @@ function Board() {
     setLoading(false);
   }, []);
 
-  console.log('posts:', posts);
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+  console.log('currentPosts:', currentPosts);
 
   return (
-    <div>
-      <Posts posts={posts} loading={loading}></Posts>
-    </div>
+    <>
+      <Posts posts={currentPosts} loading={loading}></Posts>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={setCurrentPage}
+      ></Pagination>
+    </>
   );
 }
 
